6b4d60e0a8317e97028e194839c81d82edf107a1
Tim Destan <tim.destan@gmail.com>
2017-12-03 17:10:04 -0500

    Swap profiling gem
    Old one does not seem to work on modern rubies.

--- prof.rb
+++ prof.rb
@@ -4,32 +4,35 @@
 require_relative 'tree'
 require_relative 'cyk'
 
-require 'perftools'
+require 'ruby-prof'
 
 TRAINING_FILENAME = 'f2-21.train.parse.noLEX'
 TEST_FILENAME = 'f2-21.test.parse.noLEX'
 
 # Reads an array of trees from the given filename.
-def read_trees(filename)
+def read_trees(filename, limit=nil)
   log("Reading #{filename}...")
   File.open(filename, "r") do |file|
-    file.readlines.collect do |line|
+    lines = file.readlines
+    lines = lines.take(limit) unless limit.nil?
+    lines.map do |line|
       Tree.from_string(line)
     end
   end
 end
 
-# read trees
-training_trees = read_trees(TRAINING_FILENAME)
-test_trees = read_trees(TEST_FILENAME)
-parser = CYKParser.new(training_trees)
-trees = test_trees
-
-PerfTools::CpuProfiler.start('/tmp/parser_profile') do
-  (0...4).each do |number|
-    tree = trees[number]
-    pos = tree.get_leaves()
-    guess, _ = parser.parse(pos)
-    guess.unfactor() unless guess.nil?
-  end
+result = RubyProf.profile do
+  training_trees = read_trees(TRAINING_FILENAME, 1000)
+  # test_trees = read_trees(TEST_FILENAME, limit=1000)
+  parser = CYKParser.new(training_trees)
 end
+
+# test_trees.each do |tree|
+#   pos = tree.get_leaves()
+#   guess, _ = parser.parse(pos)
+#   guess.unfactor() unless guess.nil?
+# end
+
+puts "Profile for reading trees"
+printer = RubyProf::FlatPrinter.new(result)
+printer.print(STDOUT)
